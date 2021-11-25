5daad8fe877dadc785f58e3074e070bf2b209385
socratone <gim2origin@gmail.com>
2021-11-25 21:46:24 +0900

    feat: unknown 예제 추가

--- app.ts
+++ app.ts
@@ -1,1 +1,10 @@
-console.log('hello world!');
+// unknown
+let unknownVar: unknown;
+let stringVar: string;
+
+stringVar = '글자';
+unknownVar = 10;
+
+if (typeof unknownVar === 'string') {
+  stringVar = unknownVar; // unknow이 string일 때에만 string으로 할당한 변수에 값을 넣을 수 있다.
+}
