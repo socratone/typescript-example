6d6bdb5c4061351eb40925c84dd188959f93211d
socratone <gim2origin@gmail.com>
2021-11-25 21:58:26 +0900

    feat: never 예제 추가

--- app.ts
+++ app.ts
@@ -8,3 +8,12 @@ unknownVar = 10;
 if (typeof unknownVar === 'string') {
   stringVar = unknownVar; // unknow이 string일 때에만 string으로 할당한 변수에 값을 넣을 수 있다.
 }
+
+// never
+function generateError(message: string): never {
+  throw new Error(message); // 에러가 발생하여 함수 실행이 중단되는 경우 never를 리턴한다.
+}
+
+try {
+  generateError('에러가 발생했습니다.');
+} catch {}
