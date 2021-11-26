d2bbc30199c6bbf0a5d772e7c358b69942c25026
socratone <gim2origin@gmail.com>
2021-11-27 00:07:36 +0900

    feat: getter와 setter 예제 코드 추가

---
+++ src/classGetterSetter.ts
@@ -0,0 +1,20 @@
+class Lesson {
+  private name: string;
+
+  constructor(name: string) {
+    this.name = name;
+  }
+
+  get recentName() {
+    return this.name;
+  }
+
+  set recentName(value: string) {
+    this.name = value;
+  }
+}
+
+const lesson = new Lesson('Math');
+console.log(lesson.recentName);
+lesson.recentName = 'English';
+console.log(lesson.recentName);
