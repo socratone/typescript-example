fa5d038ed5f79a59af0bd2b07b05c9da09c68bab
socratone <gim2origin@gmail.com>
2021-11-27 19:59:24 +0900

    fix: interface로 상속하는 방법 추가

--- src/classInterface.ts
+++ src/classInterface.ts
@@ -7,6 +7,7 @@ interface Teachable {
   teach: () => void;
 }
 
+// implements에는 여러 interface를 넣을 수 있다.
 class Teacher implements Person, Teachable {
   name: string;
   age: number;
@@ -18,5 +19,25 @@ class Teacher implements Person, Teachable {
 
   teach() {
     console.log('teach');
+  }
+}
+
+// 위처럼 따로 분리하지 않고 상속 받아 합쳐서 interface를 정의할 수도 있다.
+// class와 달리 interface는 둘 이상을 상속할 수도 있다.
+interface SingablePerson extends Person {
+  sing: () => void;
+}
+
+class Singer implements SingablePerson {
+  name: string;
+  age: number;
+
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
+  }
+
+  sing() {
+    console.log('sing');
   }
 }
