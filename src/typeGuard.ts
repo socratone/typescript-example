febcd3b8e62d35d60f6cdf406317dc710a679057
socratone <gim2origin@gmail.com>
2021-11-29 22:08:52 +0900

    feat: type guard 추가

---
+++ src/typeGuard.ts
@@ -0,0 +1,52 @@
+// typeof를 이용해서
+function add(a: string | number, b: string | number) {
+  if (typeof a === 'string' || typeof b === 'string') {
+    return a.toString() + b.toString();
+  }
+  return a + b;
+}
+
+// in을 이용해서
+type Candle = {
+  name: string;
+  brightness: string;
+};
+
+type Lighter = {
+  name: string;
+  fuel: string;
+};
+
+function getProperty(thing: Candle | Lighter) {
+  if ('brightness' in thing) {
+    return thing.brightness;
+  }
+  if ('fuel' in thing) {
+    return thing.fuel;
+  }
+}
+
+// instanceof를 이용해서
+class FootballPlayer {
+  kick() {
+    console.log('kick');
+  }
+}
+
+class BaseballPlayer {
+  hit() {
+    console.log('hit');
+  }
+}
+
+const player1 = new FootballPlayer();
+const player2 = new BaseballPlayer();
+
+function play(player: FootballPlayer | BaseballPlayer) {
+  if (player instanceof FootballPlayer) {
+    player.kick();
+  }
+  if (player instanceof BaseballPlayer) {
+    player.hit();
+  }
+}
